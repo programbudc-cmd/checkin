4e976d07820540601d6ad76044d002477c6f8e64
programbudc-cmd <program.budc@cpird.in.th>
2025-09-10 16:32:24 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,8 +1,9 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbxERjBKINxlVkl2ymc7vFeOm26ljWtq96ZfR6itidJOILC-fFsgaYGuD9kTuEhu8J0LBQ/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbx7KJEAr6q8fhPKYmQAauIaciTopX3tFm4UOkOXTi9W674MiM2JmIY6v_uZ2YdqzP2jXw/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby7CDeSx6mGr-4SpHSDT1TQLwlUUM49TywbOe7B-3idom32N4E0NzR_Etr3VuImMZI2/exec';
 //LIFF_ID
     const LIFF_ID = '2008088008-48JzVeeQ'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008088008-jWGqwvvn'; // LIFF ID history
     const LIFF_REGISTER = '2008088008-RGaXeww4'; // LIFF ID register
+
 
